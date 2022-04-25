63300a3a02d000b97b838b48b8587e6bccb62a59
CactusLoword <97003973+CactusLoword@users.noreply.github.com>
2022-04-25 01:29:47 +0100

    Add files via upload

---
+++ cactuslawand/randomshop/random.js
@@ -0,0 +1,73 @@
+
+import * as stories from "../stories.js";
+
+var physics={
+  "Matter and Radiation":"matter.txt",
+  "Quarks and Leptons":"quarks.txt",
+  "Quantum Phenomena":"phenomena.txt",
+  "Mechanics: Forces in equilibrium":"equilibrium.txt",
+  "Waves":"waves.txt",
+  "Mechanics in motion":"motion.txt",
+  "Optics":"optics.txt",
+}
+
+// console.log(physics["Waves"]);
+// for(let key in physics){
+//   physics[key]="physics/"+physics[key]
+// }
+// console.log(physics["Waves"])
+
+var chemistry={
+  // "Chapter 2: Ions, atoms and compounds":"chap.txt",
+  "Chapter 3: Amount of substance":"chap3.txt",
+  "Chapter 4: Acids and Redox":"chap4.txt",
+  "Chapter 5: Electrons and Bonding":"chap5.txt",
+  "Chapter 6: Shapes of molecules and intermolecular forces":"chap6.txt",
+  "Chapter 7: Periodicity":"chap7.txt",
+  "Chapter 9: Enthalpy":"chap9.txt",
+  // "Chapter 11: Basic Concepts of Organic Chemistry":"chap.txt",
+  "Chapter 12: Alkanes":"chap12.txt",
+}
+
+// for(let key in chemistry){
+//   chemistry[key]="chemistry/"+chemistry[key]
+// }
+
+var computing={
+  "Hardware":"hardware.txt",
+  "Software":"software.txt",
+  "Algorithms and Programming":"prog.txt",
+  // "Data types":"",
+}
+
+var maths={
+  "Trig and Vectors":"trig.txt",
+  "Differentiation":"differentiation.txt",
+  "Algebra and functions":"algebra.txt",
+  "Exponentials and Logarithms":"explog.txt",
+  "Kinematics":"kinematics.txt",
+  "Transforming/ sketching graphs":"transformers.txt",
+  // "Proof":"",
+  // "Integration":"",
+  // "Sampling":"",
+}
+
+var listStory={
+  "physlist":physics,
+  "chemlist":chemistry,
+  "complist":computing,
+  "mathslist":maths
+}
+
+var storyNames={
+  "storyNamePhys":"Physics Reviews",
+  "storyNameChem":"Chemistry Reviews",
+  "storyNameComp":"Computing Reviews",
+  "storyNameMaths":"Maths Reviews"
+}
+
+stories.entryScreen()
+stories.contentsSetup("physlist",storyNames,listStory/*,pearChapters*/,"storyNamePhys"/*,"physics/desc.txt"*/,"physics/")
+stories.contentsSetup("chemlist",storyNames,listStory/*,pearChapters*/,"storyNameChem"/*,"chemistry/desc.txt"*/,"chemistry/")
+stories.contentsSetup("complist",storyNames,listStory/*,pearChapters*/,"storyNameComp"/*,"computing/desc.txt"*/,"computing/")
+stories.contentsSetup("mathslist",storyNames,listStory/*,pearChapters*/,"storyNameMaths"/*,"maths/desc.txt"*/,"maths/")
